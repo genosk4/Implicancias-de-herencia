ac8cd98e34d84ff8623d36d67c1901563abf74ab
genosk4 <lincopidaniel7@gmail.com>
2025-11-05 22:34:36 -0300

    clases completas

--- src/Departamento.java
+++ src/Departamento.java
@@ -1,10 +1,8 @@
 public class Departamento {
     private String nombre;
-    private String carreras;
 
     public Departamento(String nombre) {
         this.nombre = nombre;
-        this.carreras = carreras;
     }
 
 }
